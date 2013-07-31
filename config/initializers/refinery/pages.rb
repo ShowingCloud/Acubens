fb0d7876101d76cef9d26a0067c8be56dbd98eac
xp <superking1991@126.com>
2013-07-31 15:44:57 +0000

    first day

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -9,7 +9,7 @@ Refinery::Pages.configure do |config|
   # config.default_parts = ["Body", "Side Body"]
 
   # Configure whether to allow adding new page parts
-  # config.new_page_parts = false
+  config.new_page_parts = true
 
   # Configure whether to enable marketable_urls
   # config.marketable_urls = true
@@ -39,13 +39,13 @@ Refinery::Pages.configure do |config|
   # Set this to true to fully expand the page hierarchy in the admin
   # config.auto_expand_admin_tree = true
 
-  # config.layout_template_whitelist = ["application"]
+  config.layout_template_whitelist = ["application","testlayout"]
 
-  # config.view_template_whitelist = ["home", "show"]
+  config.view_template_whitelist = ["home", "exchangeverify","resettel","resetpwd","membersystem","changeinfo","firstlogin","mailverify","pwdverify","memberinfo","showexchangehistory","exchangepoints","exchangeverifyjx","myorder","mypoints"]
 
-  # config.use_layout_templates = false
+  config.use_layout_templates = true
 
-  # config.use_view_templates = false
+  config.use_view_templates = true
 
   # config.page_title = {:chain_page_title=>false, :ancestors=>{:separator=>" | ", :class=>"ancestors", :tag=>"span"}, :page_title=>{:class=>nil, :tag=>nil, :wrap_if_not_chained=>false}}
 
