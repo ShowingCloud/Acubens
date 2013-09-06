a2ad840b38da5e2d6b5ffce5e7762aae0df519cc
WANG Guoqin <wangguoqin1001@gmail.com>
2013-09-06 23:50:53 +0800

    Localization

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,15 +1,15 @@
 # encoding: utf-8
 
 Refinery::I18n.configure do |config|
-  # config.enabled = true
+  config.enabled = true
 
-  # config.default_locale = :en
+  config.default_locale = :"zh-CN"
 
-  # config.current_locale = :en
+  config.current_locale = :"zh-CN"
 
-  # config.default_frontend_locale = :en
+  config.default_frontend_locale = :"zh-CN"
 
-  # config.frontend_locales = [:en]
+  config.frontend_locales = [:en, :"zh-CN"]
 
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}
 end
