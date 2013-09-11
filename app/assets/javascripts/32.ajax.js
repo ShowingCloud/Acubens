975e0086588bfb9f73d3569e4dc9d7c68b7373a1
zhangjay0710 <zhangjay0710@163.com>
2013-09-12 01:05:15 +0800

    left

--- app/assets/javascripts/32.ajax.js
+++ app/assets/javascripts/32.ajax.js
@@ -8,3 +8,17 @@ function login(){
 		});
 	//alert("xxx");
 }
+
+function register(){
+	$("#popup_dialog div").load ('/home/login-page .tabbed_area-login');
+	$('#popup_shadow').css("display", "block");
+	$('body').css ("overflow", "hidden");
+	
+	$('#popup_shadow').click (function() {
+			location.href = "/";
+		});
+	$('#content_2').css("display", "block");
+	$('#content_1').css("display", "none");
+	$("#tab_2").addClass("active");
+	
+}
