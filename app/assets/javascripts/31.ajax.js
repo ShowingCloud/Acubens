ef50a4d3dc2a9bd183eda3ce2033346d3406f7b1
WANG Guoqin <wangguoqin1001@gmail.com>
2013-09-14 13:09:31 +0800

    get detailed info

--- app/assets/javascripts/31.ajax.js
+++ app/assets/javascripts/31.ajax.js
@@ -3,13 +3,10 @@ function sendfirstinfo(){
 		alert("请输入您的名字");
 		return;
 	}
-	
-	var year = $('#year').val();
-		
-	var month = $('#month').val();
-		
-	var day = $('#day').val();
 
+	var year = $('#year').val();
+	var month = $('#month').val();
+	var day = $('#day').val();
 
 	if ($('input[name="sex"]:checked').length > 0)
 		var gender = $('input[name="sex"]:checked').val();
@@ -17,15 +14,14 @@ function sendfirstinfo(){
 		alert ("请选择您的性别");
 		return;
 	}
-	
-	
+
 	if ($('input[name="magazine"]:checked').length > 0)
 	   var magazine = $('input[name="magazine"]:checked').val();
 	else {
 		alert ("请选择是否订阅电子杂志");
 		return;
 	}
-	
+
 	if ($('#province').val().length > 0)
 	   var province = $('#province').val();
 	else {
@@ -44,7 +40,7 @@ function sendfirstinfo(){
 		alert ("请选择您所在的区");
 		return;
 	}	
-	
+
 	if($('#address').val().length < 0){
 		alert ("请输入您的详细地址");
 		return;
@@ -52,19 +48,19 @@ function sendfirstinfo(){
 		alert  ("请输入邮政编码");
 		return;
 	}
-		
+
 	var extensionnumber = 0;
 		if ($('#extensionnumber').val().length > 0)
 		extensionnumber = $('#extensionnumber').val();
-		
+
 	var weibo = 0;
 		if ($('#weibo').val().length > 0)
 		weibo = $('#weibo').val();
-			
+
 	var weixin = 0;
 		if ($('#weixin').val().length > 0)
 		weibo = $('#weixin').val();
-		
+
    
 	$.ajax({
 		url:        "/membership/fillinfo.json",
@@ -95,7 +91,6 @@ function sendfirstinfo(){
 }
 
 function sendchangeinfo(){
-	
 
 	if ($('input[name="magazine"]:checked').length > 0)
 	   var magazine = $('input[name="magazine"]:checked').val();
@@ -103,7 +98,7 @@ function sendchangeinfo(){
 		alert ("请选择是否订阅电子杂志");
 		return;
 	}
-	
+
 	if ($('#province').val().length > 0)
 	   var province = $('#province').val();
 	else {
@@ -122,7 +117,7 @@ function sendchangeinfo(){
 		alert ("请选择您所在的区");
 		return;
 	}	
-	
+
 	if($('#address').val().length < 0){
 		alert ("请输入您的详细地址");
 		return;
@@ -130,15 +125,15 @@ function sendchangeinfo(){
 		alert  ("请输入邮政编码");
 		return;
 	}
-	
+
 	var extensionnumber = 0;
 		if ($('#extensionnumber').val().length > 0)
 		extensionnumber = $('#extensionnumber').val()
-	
+
 	var weibo = 0;
 		if ($('#weibo').val().length > 0)
 		weibo = $('#weibo').val()
-	
+
 	var weixin = 0;
 		if ($('#weixin').val().length > 0)
 		weibo = $('#weixin').val()
@@ -179,59 +174,115 @@ function getmeminfo() {
 	}).done (function (resp) {
 		if (parseInt (resp.status) == 1) {
 			var ret = resp.return_value;
-			
-			
-            $('#email').html (ret.email == null ? "" : ret.email);
-			$('#name').html (ret.nick_name == null ? "" : ret.nick_name);
-			/*生日*/
-			if(ret.year != null && ret.month != null && ret.day != null)
-				$('#birthday').html (ret.year+"年"+ret.month+"月"+ret.day+"日");			
-			
-			if (ret.gender_id == 69)
-			{
-				$('#gender').html ("男");
-				$('input[id="male"]').attr("checked","checked") == true;
+	
+			var email = ret.email == null ? "" : ret.email;
+			var nickname = ret.nick_name == null ? "" : ret.nick_name;
+			var address = ret.address == null ? "" : ret.address;
+			var mobile = ret.mobile == null ? "" : ret.mobile;
+			var taobao = ret.column6 == null ? "" : ret.column6;
+			var yihao = ret.column7 == null ? "" : ret.column7;
+			var weibo = ret.column4 == null ? "" : ret.column4;
+			var weixin = ret.column5 == null ? "" : ret.column5;
+			var password = "00000000000000000000";
+
+			if (ret.gender_id == 69) {
+				var gender = "男";
+				var gender_str = "male";
+			} else if (ret.gender_id == 70) {
+				var gender = "女";
+				var gender_str = "female";
+			} else {
+				var gender = "";
+				var gender_str = "";
 			}
-			else if (ret.gender_id == 70)
-			{
-				$('#gender').html ("女");
-				$('input[id="female"]').attr("checked","checked") == true;
+
+			if (ret.column1 == "是") {
+				var subscription = "订阅";
+				var subscriptoin_str = "subscribe";
+			} else if (ret.column1 == "否") {
+				var subscription = "不订阅";
+				var subscription_str = "nosubscribe";
+			} else {
+				var subscription = "";
+				var subscription_str = "";
 			}
-			if (ret.column1 == "是")
-			{
-				$('#magazine').html ("订阅");
-				$('input[id="subscribe"]').attr("checked","checked") == true;
+
+			if (ret.column3 == null)
+				var phone = "";
+			else {
+				var phone = ret.column3.split('-');
+
+				if (phone[1] == "")
+					phone = "";
+				else if (phone[0] == "" && phone[2] == "")
+					phone = phone[1];
+				else if (phone[0] == "")
+					phone = phone[1] + '-' + phone[2];
+				else if (phone[2] == "")
+					phone = phone[0] + '-' + phone[1];
+				else
+					phone = phone[0] + '-' + phone[1] + '-' + phone[2];
 			}
-			else if (ret.column1 == "否")
-			{
-				$('#magazine').html ("不订阅");
-				$('input[id="notsubscribe"]').attr("checked","checked") == true;
+
+			if (ret.birthday_dt == null)
+				var birthdate = "";
+			else {
+				var birthdate = ret.birthday_dt.split('T')[0].split('-');
+
+				if (birthdate[0] == null || birthdate[1] == null || birthdate[2] == null)
+					birthdate = "";
+				else
+					birthdate = birthdate[0]+"年"+birthdate[1]+"月"+birthdate[2]+"日";			
 			}
-			
-			/*省市区待添加*/	
-			
-			$('#address').html (ret.address == null ? "" : ret.address);
-			$('#mobile').html (ret.mobile == null ? "" : ret.mobile);
-			/*固定电话*/
-			var phone = ret.column3 == null ? "" : ret.column3;
-			
-			var n = phone.split("-");
-			//alert(n[0]);
-			if(n[1] != null)
-				$('#tel').html(n[0]+"-"+n[1]+"-"+n[2]);
-			
-			$('#taobao').html (ret.column6 == null ? "" : ret.column6);
-			$('#yihao').html (ret.column7 == null ? "" : ret.column7);
-			$('#weibo').html (ret.column4 == null ? "" : ret.column4);
-			$('#weixin').html (ret.column5 == null ? "" : ret.column5);
-			
-			$('input[id="email"]').val(ret.email == null ? "" : ret.email);
-			$('input[id="password"]').val("00000000000000000000");
-			$('input[id="identification"]').val("00000000000000000000");
-			$('input[id="name"]').val(ret.nick_name == null ? "" : ret.nick_name);
-			$('input[id="mobile"]').val(ret.mobile == null ? "" : ret.mobile);
-			$('input[id="address"]').val(ret.address == null ? "" : ret.address);
-							
+
+			if ($('#email').length)
+				$('#email').html (email);
+			if ($('#name').length)
+				$('#name').html (nickname);
+			if ($('#birthday').length)
+				$('#birthday').html (birthdate);
+			if ($('#gender').length)
+				$('#gender').html (gender);
+
+			if (gender_str != "")
+				if ($('input[id="' + gender_str + '"]').length)
+					$('input[id="' + gender_str + '"]').attr("checked","checked") == true;
+
+			if ($('#magazine').length)
+				$('#magazine').html (subscription);
+
+			if (subscription_str != "")
+				if ($('input[id="' + subscription_str + '"]').length)
+					$('input[id="' + subscription_str + '"]').attr("checked","checked") == true;
+
+			if ($('#address').length)
+				$('#address').html (address);
+			if ($('#mobile').length)
+				$('#mobile').html (mobile);
+			if ($('#tel').length)
+				$('#tel').html (phone);
+			if ($('#taobao').length)
+				$('#taobao').html (taobao);
+			if ($('#yihao').length)
+				$('#yihao').html (yihao);
+			if ($('#weibo').length)
+				$('#weibo').html (weibo);
+			if ($('#weixin').length)
+				$('#weixin').html (weixin);
+
+			if ($('input[id="email"]').length)
+				$('input[id="email"]').val (email);
+			if ($('input[id="password"]').length)
+				$('input[id="password"]').val (password);
+			if ($('input[id="identification"]').length)
+				$('input[id="identification"]').val (password);
+			if ($('input[id="name"]').length)
+				$('input[id="name"]').val (nickname);
+			if ($('input[id="mobile"]').length)
+				$('input[id="mobile"]').val (mobile);
+			if ($('input[id="address"]').length);
+				$('input[id="address"]').val (address);
+
 		} else {
 			if (resp.description != null)
 				alert (resp.description);
@@ -243,45 +294,47 @@ function getmeminfo() {
 	});
 }
 
+
 function sendsurvey(){
-	
+
 	var skin_survey = {
 		"user" : "15901940875",
-	
+
 		"style": $('input[name="type"]:checked').val(),
-		
+
 		"care": $('input[name="importance"]:checked').val(),
-		
+
 		"problems" : $("input[name='problem']:checked").val(),
     	
-	"time" : $('input[name="time"]:checked').val(),
-	
-	"procedures" : $("input[name='step']:checked").val(),
+		"time" : $('input[name="time"]:checked').val(),
+
+		"procedures" : $("input[name='step']:checked").val(),
        
-    "effects" : $("input[name='mostimp']:checked").val(),
+	    "effects" : $("input[name='mostimp']:checked").val(),
     
-    "shortcomings" : $("input[name='reason']:checked").val(),
+	    "shortcomings" : $("input[name='reason']:checked").val(),
    	
-	"cost" : $('input[name="cost"]:checked').val(),
-		
-	"markets" : $("input[name='channel']:checked").val(),
+		"cost" : $('input[name="cost"]:checked').val(),
+
+		"markets" : $("input[name='channel']:checked").val(),
     
-    "factors" : $("input[name='affect']:checked").val(),
+	    "factors" : $("input[name='affect']:checked").val(),
     
-    "brands" : $("input[name='brand']:checked").val(),
-		
-	"importance" : $('input[name="brandimp"]:checked').val(),
-	
-	"source" : $('input[name="know"]:checked').val(),
-		
-	"ways" : $("input[name='way']:checked").val(),
-		
-	"infos" : $("input[name='info']:checked").val(),
-		
-	"offline" : $('input[name="activ"]:checked').val(),
-		
-	"age" : $('input[name="age"]:checked').val()	}
-				
+	    "brands" : $("input[name='brand']:checked").val(),
+
+		"importance" : $('input[name="brandimp"]:checked').val(),
+
+		"source" : $('input[name="know"]:checked').val(),
+
+		"ways" : $("input[name='way']:checked").val(),
+
+		"infos" : $("input[name='info']:checked").val(),
+
+		"offline" : $('input[name="activ"]:checked').val(),
+
+		"age" : $('input[name="age"]:checked').val()
+	}
+
 	alert (skin_survey);
 	$.ajax({
 		url:        "/skin_surveys.json",
@@ -289,7 +342,7 @@ function sendsurvey(){
 		dataType:   "json",
 		data:       {
 			skin_survey: skin_survey,
-			
+
 		}
 	}).done (function (resp) {
 		if(parseInt (resp.status) == 1) {
@@ -308,18 +361,18 @@ function sendsurvey(){
 
 
 function addaddress(){
-	
+
 
 	if ($('#name').val().length < 2){
 		alert("请输入您的名字");
 		return;
 	}
-	
+
 	if ($('#mobile').val().length < 2){
 		alert("请输入您的名字");
 		return;
 	}
-	
+
 	if ($('#province').val().length > 0)
 	   var province = $('#province').val();
 	else {
@@ -338,7 +391,7 @@ function addaddress(){
 		alert ("请选择您所在的区");
 		return;
 	}	
-	
+
 	if($('#address').val().length < 0){
 		alert ("请输入您的详细地址");
 		return;
@@ -346,7 +399,7 @@ function addaddress(){
 		alert  ("请输入邮政编码");
 		return;
 	}
-	
+
 	var extensionnumber = 0;
 		if ($('#extensionnumber').val().length > 0)
 		extensionnumber = $('#extensionnumber').val()
@@ -378,5 +431,3 @@ function addaddress(){
 		alert ("请求发送失败，请稍候再试");
 	});
 }
-
-
