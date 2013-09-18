dd94c70867c57149d73494722ed0d848fd2d574e
WANG Guoqin <wangguoqin1001@gmail.com>
2013-09-18 11:56:52 +0800

    Set RAILS_ASSET_ID to expire the assets

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Acubens::Application.initialize!
+
+ENV['RAILS_ASSET_ID'] = `git log -1 --pretty=format:"%h"`
