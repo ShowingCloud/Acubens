b95d444002f150b00868fe6f4a8a1980ef57ba85
WANG Guoqin <wangguoqin1001@gmail.com>
2013-09-18 17:27:59 +0800

    Google Analytics track number renewed

--- app/assets/javascripts/31.googleanalytics.js
+++ app/assets/javascripts/31.googleanalytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-44112986-1', 'gelnic.com.cn');
+ga('create', 'UA-43691779-2', 'gelnic.com.cn');
 ga('send', 'pageview');
