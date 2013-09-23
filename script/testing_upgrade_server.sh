04339c5f0fdd2be8aba484ae6d4eca55fca01ecd
Gelnic Production <customer@showingcloud.com>
2013-09-23 11:41:27 +0800

    Upgrading scripts for testing and production

---
+++ script/testing_upgrade_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+git pull
+rm -rf db/datadump
+rake db:data:dump_dir dir="datadump"
+git commit -a -m '$(date) 800ts modification'
+git push
+read -n1 -r -p 'Create pull request (if needed), approve it and press any key to continue' key
+
+git pull
+git fetch upstream
+git merge upstream/master
+rake db:migrate
+rake db:seed
+rake db:data:load_dir dir="datadump"
+git push
