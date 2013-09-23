f7ed774bbe68d74c58a554f85f04e3e6d531fb4a
Gelnic Production <customer@showingcloud.com>
2013-09-23 12:34:49 +0800

    Mon Sep 23 12:34:49 CST 2013
    800ts modification

--- script/testing_upgrade_server.sh
+++ script/testing_upgrade_server.sh
@@ -5,7 +5,7 @@ set -x
 git pull
 rm -rf db/datadump
 rake db:data:dump_dir dir="datadump"
-git commit -a -m '$(date)' -m '800ts modification'
+git commit -a -m "$(date)" -m '800ts modification'
 git push
 read -n1 -r -p 'Create pull request (if needed), approve it and press any key to continue' key
 
