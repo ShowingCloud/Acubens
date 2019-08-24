7d871bdb131e9168f162ace555df495c5656d0a9
www-data @ SCS.IM <www-data@scs.im>
2019-08-24 02:08:38 -0400

    Updated for server deployment

--- script/production_start_server.sh
+++ script/production_start_server.sh
@@ -3,4 +3,4 @@
 set -x
 
 thin stop
-thin -d -p 3200 -s3 -e production start
+thin -d -p 3250 -e production --tag "Gelnic Production" start
--- script/production_upgrade_server.sh
+++ script/production_upgrade_server.sh
@@ -8,7 +8,7 @@ export RAILS_ENV
 git pull
 rake db:migrate
 rake db:data:load_dir dir="datadump"
-rm -rf vendor/assets/images
-cp -r ../testing/vendor/assets/images/ vendor/assets/images/
+#rm -rf vendor/assets/images
+#cp -r ../testing/vendor/assets/images/ vendor/assets/images/
 rake assets:clean
 rake assets:precompile
